dc21a3239d3168258300de3d3a6ba91932d5686e
jamerson-macedo <jamerson_macedo@hotmail.com>
2025-03-10 16:28:21 -0300

    Initial Commit

---
+++ PinterestClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinterestClone
+//
+//  Created by Jamerson Macedo on 10/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PinterestClone/PinterestCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestCloneApp.swift
+//  PinterestClone
+//
+//  Created by Jamerson Macedo on 10/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PinterestCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
